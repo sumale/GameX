ea6f16018e0a22f410c5202aedc01b5a675b1ed7
iXinwei <zhang010917@outlook.com>
2016-01-28 23:53:31 +0800

    no message

---
+++ UI/MainForm.Designer.cs
@@ -0,0 +1,86 @@
+﻿namespace UI
+{
+    using System.Drawing;
+    using System.Drawing.Drawing2D;
+    partial class MainForm
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.SuspendLayout();
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 18F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(942, 493);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "MainForm";
+            this.Text = "GameX";
+            this.Paint += new System.Windows.Forms.PaintEventHandler(this.MainForm_Paint);
+            this.MouseDown += new System.Windows.Forms.MouseEventHandler(this.MainForm_MouseDown);
+            this.MouseMove += new System.Windows.Forms.MouseEventHandler(this.MainForm_MouseMove);
+            this.MouseUp += new System.Windows.Forms.MouseEventHandler(this.MainForm_MouseUp);
+            this.ResumeLayout(false);
+
+        }
+        #endregion
+        private void SetFormSize()
+        {
+            int X, Y;
+            X = System.Windows.Forms.Screen.PrimaryScreen.Bounds.Width;
+            Y = System.Windows.Forms.Screen.PrimaryScreen.Bounds.Height;
+            float a = X / Y;
+            if (a == 16 / 9)
+            {
+                if (X < 1400 && Y < 800)
+                {
+                    this.Width = 960;
+                    this.Height = 540;
+                }
+                else
+                {
+                    this.Width = 1280;
+                    this.Height = 720;
+                }
+            }
+            else if (a == 4 / 3)
+                ;// 4:3
+            else if (a == 16 / 10)
+                ;// 16:10
+            else if (a == 5 / 4)
+                ;// 5:4
+
+
+        }
+        private void InitializeControls()
+        {
+
+        }
+        
+    }
+}
+
---
+++ UI/MainForm.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+//using System.Drawing;
+using System.Drawing.Drawing2D;
+
+namespace UI
+{
+    public partial class MainForm : Form
+    {
+        Point mouseOff;//鼠标移动位置变量
+        bool leftFlag;//标签是否为左键
+
+        public MainForm()
+        {
+            InitializeComponent();
+            SetFormSize();      //to do
+            InitializeControls();//to do
+        }
+        private void MainForm_MouseDown(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Left)
+            {
+                mouseOff = new Point(-e.X, -e.Y); //得到变量的值
+                leftFlag = true;                  //点击左键按下时标注为true;
+            }
+        }
+
+        private void MainForm_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (leftFlag)
+            {
+                Point mouseSet = Control.MousePosition;
+                mouseSet.Offset(mouseOff.X, mouseOff.Y);  //设置移动后的位置
+                Location = mouseSet;
+            }
+        }
+
+
+        private void MainForm_MouseUp(object sender, MouseEventArgs e)
+        {
+            if (leftFlag)
+            {
+                leftFlag = false;//释放鼠标后标注为false;
+            }
+        }
+
+        private void MainForm_Paint(object sender, PaintEventArgs e)
+        {
+            Graphics G = e.Graphics;
+            G.SmoothingMode = SmoothingMode.AntiAlias;
+            G.DrawString("This is GameX APP initialization.\n这是GameX初始化界面。", new Font("微软雅黑", 32), Brushes.Black, new PointF(0, 0));
+            G.FillRectangle(new SolidBrush(Color.FromArgb(255,Color.Azure)), new Rectangle((int)(0.12F * this.Width), (int)(0.25F * this.Height),128*(this.Width / 1280),128*(this.Height / 720)));
+            
+            G.Dispose();
+        }
+    }
+}
